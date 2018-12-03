96e8fae4337067bebed3e38e21b8635b01266e68
MariamHmaida <hmaida.mariama@gmail.com>
2018-12-03 12:27:39 +0100

    dernière correction

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -66,8 +66,8 @@ public class Main {
 		Iterables.addAll(students, repo);
 
 		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
-				return students.get(i-1);
+			if (students.get(i).getId()== studentId) {
+				return students.get(i);
 			}
 		}
 
