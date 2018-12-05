d49c51b92a58515ab7b0505ce080ff448569f5ee
Androunie <andrea.perri.chretien@gmail.com>
2018-12-05 19:50:55 +0100

    2eme bug

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -1,35 +1,21 @@
 package fr.pantheonsorbonne.miage;
 
+//imports unused removed
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
 import java.util.NoSuchElementException;
-import java.util.logging.Logger;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletRegistration;
 
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
 
-import com.google.common.collect.Collections2;
+
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
 
 /**
@@ -37,12 +23,14 @@ import com.google.common.io.ByteStreams;
  *
  */
 public class Main {
-	public static final String HOST = "localhost";
-	public static final int PORT = 7000;
-	private static final Logger logger = Logger.getLogger(Main.class.getName());
+	//public static final String HOST = "localhost";
+	//public static final int PORT = 7000;
+	//private static final Logger logger = Logger.getLogger(Main.class.getName());
 	private static StudentRepository studentRepo = StudentRepository.withDB("src/main/resources/students.db");
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
+
+	//exception unused removed
+	public static void main(String[] args)  {
 
 		HttpServer server = HttpServer.createSimpleServer();
 		addRootPath(server, "/home");
@@ -55,16 +43,17 @@ public class Main {
 			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
 			System.out.println("Press any key to stop the server...");
 			System.in.read();
-		} catch (Exception e) {
+		} catch (Exception e) { //exception non used
 			System.err.println(e);
 		}
 	}
 
-	protected static Student getStudentData(int studentId, StudentRepository repo) {
+	private static Student getStudentData(int studentId, StudentRepository repo) {
 		// create an arrayList of the students, because iterables are too hard
 		ArrayList<Student> students = new ArrayList<>();
 		Iterables.addAll(students, repo);
 
+		//index changed to get the appropriate name
 		for (int i = 1; i <= students.size(); i++) {
 			if (i == studentId) {
 				return students.get(i-1);
@@ -75,7 +64,7 @@ public class Main {
 
 	}
 
-	protected static void handleResponse(Response response, int studentId) throws IOException {
+	private static void handleResponse(Response response, int studentId) throws IOException {
 
 		response.setContentType("application/pdf");
 
@@ -91,7 +80,7 @@ public class Main {
 		return;
 	}
 
-	protected static void addDiplomaPath(HttpServer server, String path) {
+	private static void addDiplomaPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
 				// get the id of the student
@@ -106,7 +95,7 @@ public class Main {
 		}, path);
 	}
 
-	protected static void addRootPath(HttpServer server, String path) {
+	private static void addRootPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
 
@@ -114,8 +103,7 @@ public class Main {
 				sb.append("<!DOCTYPE html><head><meta charset='utf-8'></head><body><h1>Liste des diplômés</h1><ul>");
 				for (Student stu : StudentRepository.withDB("src/main/resources/students.db")) {
 					sb.append("<li>");
-					sb.append(
-							"<a href='/diploma/" + stu.getId() + "'>" + stu.getTitle() + ' ' + stu.getName() + "</a>");
+					final StringBuilder append = sb.append("<a href='/diploma/").append(stu.getId()).append("'>").append(stu.getTitle()).append(' ').append(stu.getName()).append("</a>");
 					sb.append("</li>");
 				}
 
