38f155f39541de7e2b8e08c15e8d0df968e63051
AurelieClaire <elja092017@gmail.com>
2018-12-05 03:06:11 +0100

    correction qualité du code

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -2,35 +2,19 @@ package fr.pantheonsorbonne.miage;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
 import java.util.NoSuchElementException;
-import java.util.logging.Logger;
-
-import javax.servlet.Servlet;
-import javax.servlet.ServletRegistration;
-
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
-
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main class.
@@ -43,7 +27,7 @@ public class Main {
 	private static StudentRepository studentRepo = StudentRepository.withDB("src/main/resources/students.db");
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
-
+        String s ="http://localhost:8080/home";
 		HttpServer server = HttpServer.createSimpleServer();
 		addRootPath(server, "/home");
 		addDiplomaPath(server, "/diploma/*");
@@ -52,11 +36,13 @@ public class Main {
 
 		{
 			server.start();
-			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
-			System.out.println("Press any key to stop the server...");
+			java.awt.Desktop.getDesktop().browse(new URI(s));
+			logger.log(Level.INFO, "Press any key to stop the server...");
 			System.in.read();
+			
 		} catch (Exception e) {
-			System.err.println(e);
+	
+		    logger.log(Level.INFO, "Erreur");
 		}
 	}
 
@@ -66,11 +52,12 @@ public class Main {
 		Iterables.addAll(students, repo);
 
 		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
+			if (students.get(i).getId() == studentId) {
 				return students.get(i);
 			}
 		}
 
+		
 		throw new NoSuchElementException();
 
 	}
@@ -88,7 +75,7 @@ public class Main {
 			}
 
 		}
-		return;
+		
 	}
 
 	protected static void addDiplomaPath(HttpServer server, String path) {
