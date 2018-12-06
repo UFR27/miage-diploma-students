487f704a73a7b59065a5e8c65b3c91335f827173
BasharAlayan <alayanbashar@gmail.com>
2018-12-06 20:51:50 +0100

    correction sonar

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
@@ -52,7 +52,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 
 		} catch (IOException e) {
 
-			throw new RuntimeException("failed to generate the file to stream to", e);
+			throw new IllegalArgumentException("failed to generate the file to stream to", e);
 		}
 
 	}
@@ -76,7 +76,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 			document.add(Image.getInstance(image.toAbsolutePath().toString()));
 
 		} catch (DocumentException | IOException e) {
-			throw new RuntimeException("failed to generate Document", e);
+			throw new IllegalArgumentException("failed to generate Document", e);
 		} finally {
 			document.close();
 		}
