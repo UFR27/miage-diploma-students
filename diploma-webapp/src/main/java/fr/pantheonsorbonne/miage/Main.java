b839cf3708021dd90c11140727052b1a55e82bc4
Bobby Sanchez <ismael.eljarrari@gmail.com>
2019-02-05 11:54:41 +0100

    ptimise import sonarlint

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -2,7 +2,6 @@ package fr.pantheonsorbonne.miage;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
