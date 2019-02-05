0b2298fda9f53fb42adffca5a011a11110638a1b
Tayfun Bilgen <tayfunbilgen0@gmail.com>
2019-02-05 11:40:16 +0100

    lint

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -2,34 +2,23 @@ package fr.pantheonsorbonne.miage;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
+
 import java.util.NoSuchElementException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletRegistration;
+
 
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
 
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
+
+
 import com.google.common.io.ByteStreams;
 
 /**
@@ -52,18 +41,19 @@ public class Main {
 
 		{
 			server.start();
-			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
-			System.out.println("Press any key to stop the server...");
+			String url = "http://localhost:8080/home";
+			java.awt.Desktop.getDesktop().browse(new URI(url));
+			logger.log(Level.ALL,"Press any key to stop the server...");
 			System.in.read();
 		} catch (Exception e) {
-			System.err.println(e);
+			logger.log(Level.ALL,e.toString());
 		}
 	}
 
 	protected static Student getStudentData(int studentId, StudentRepository repo) {
 //test		
 		for(Student stu : repo) {
-			if(stu.getId()==studenId) {
+			if(stu.getId()==studentId) {
 				return stu;
 			}
 		}
@@ -85,7 +75,7 @@ public class Main {
 			}
 
 		}
-		return;
+		
 	}
 
 	protected static void addDiplomaPath(HttpServer server, String path) {
