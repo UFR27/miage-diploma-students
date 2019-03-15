6eb4b67300b4b214f9a506a6f19a9fc9a60ddd99
Kerryo <arnaudkerry@gmail.com>
2019-03-15 22:56:42 +0100

    fixed DiplomaFileAdapter

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaFileAdapter.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaFileAdapter.java
@@ -3,7 +3,8 @@ package fr.pantheonsorbonne.miage;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.common.io.ByteStreams;
 
@@ -18,13 +19,15 @@ public class DiplomaFileAdapter extends FileGenerator<AbstractDiplomaGenerator> 
 
 	@Override
 	public void generateFile(String outputFile) {
+		Logger logger = Logger.getLogger("logger");
+		logger.isLoggable(Level.INFO, "Logger Test");
 		try (FileOutputStream fos = new FileOutputStream(outputFile)) {
 			InputStream is = this.generator.getContent();
 			ByteStreams.copy(is, fos);
 			is.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException("failed to write diploma file", e);
+			logger.isLoggable(Level.SEVERE, e.getMessage())
+			throw new StudentListException("failed to write diploma file",e);
 		}
 	}
 
