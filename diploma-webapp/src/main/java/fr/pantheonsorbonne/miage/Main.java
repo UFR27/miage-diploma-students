5c531ac43f70234d45f17886f24c5bfe70c22a56
steeve2307 <steevenalliel23@gmail.com>
2019-11-10 13:04:33 +0100

    modification de la boucle for dans la fonction getStudentData : la comparaison était incorrecte, elle comparait seulement l'indice à l'id au lieu de l'id de l'etudiant à l'indice correspondant

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -65,12 +65,12 @@ public class Main {
 		ArrayList<Student> students = new ArrayList<>();
 		Iterables.addAll(students, repo);
 
-		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
+		for (int i = 0; i <= students.size(); i++) {
+			if (students.get(i).getId() == studentId) {
 				return students.get(i);
 			}
 		}
-
+		
 		throw new NoSuchElementException();
 
 	}
