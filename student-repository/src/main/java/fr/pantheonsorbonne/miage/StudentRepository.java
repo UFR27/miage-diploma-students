0840c9fd57eedb1f9867fc2beb8740d8ad33d288
unknown <estellehu@hotmail>
2019-11-26 20:13:49 +0100

    test

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -64,8 +64,13 @@ public class StudentRepository implements Iterable<Student> {
 			
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
+<<<<<<< HEAD
 			currentIterator = parser.getRecords().stream()
 					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
+=======
+			this.currentIterator = parser.getRecords().stream()
+					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
+>>>>>>> origin/encryption
 					.map(c -> (Student) c).iterator();
 			return currentIterator;
 
