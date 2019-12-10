710670fe8b09bbe48ef3a877205e6fae5a779d6a
Nicolas Lewin <nicolas.lewin@outlook.com>
2019-12-10 22:29:17 +0100

    Corrections Sonar

---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/DBUpdateException.java
@@ -0,0 +1,29 @@
+package fr.pantheonsorbonne.miage;
+
+public class DBUpdateException extends RuntimeException {
+
+	public DBUpdateException() {
+	
+	}
+
+	public DBUpdateException(String arg0) {
+		super(arg0);
+		
+	}
+
+	public DBUpdateException(Throwable arg0) {
+		super(arg0);
+		
+	}
+
+	public DBUpdateException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+	public DBUpdateException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		
+	}
+
+}
