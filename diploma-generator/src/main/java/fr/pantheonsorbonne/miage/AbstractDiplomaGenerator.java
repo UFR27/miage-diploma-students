03d827a5c1987b3266ec5f38728d797520292bad
jugurhub <Jugurtha.Boudarene@etu.univ-paris1.fr>
2019-12-12 15:31:05 +0100

    Bug SonarLint Fixed

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
@@ -3,15 +3,14 @@ package fr.pantheonsorbonne.miage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.HashSet;
 
-import com.google.common.io.ByteStreams;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
@@ -22,7 +21,7 @@ import fr.pantheonsorbonne.miage.diploma.DiplomaSnippet;
 
 public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 
-	private Collection<DiplomaSnippet> snippets = new HashSet<>();
+
 
 	public AbstractDiplomaGenerator() {
 		super();
@@ -43,7 +42,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 	 * @see fr.pantheonsorbonne.miage.DiplomaGenerator#getContent()
 	 */
 	@Override
-	public InputStream getContent() {
+	public InputStream getContent() throws GenerateFileException, GeneratorDocumentException {
 
 		try (ByteArrayOutputStream bos = new ByteArrayOutputStream();) {
 
@@ -52,13 +51,13 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 			return new ByteArrayInputStream(bos.toByteArray());
 
 		} catch (IOException e) {
-
-			throw new RuntimeException("failed to generate the file to stream to", e);
+ 
+			throw new GenerateFileException();
 		}
 
 	}
 
-	protected void writeToStream(OutputStream os) {
+	protected void writeToStream(OutputStream os) throws GeneratorDocumentException {
 		Document document = new Document();
 	
 		try {
@@ -77,7 +76,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 			document.add(Image.getInstance(image.toAbsolutePath().toString()));
 
 		} catch (DocumentException | IOException e) {
-			throw new RuntimeException("failed to generate Document", e);
+			throw new GeneratorDocumentException();
 		} finally {
 			document.close();
 		}
