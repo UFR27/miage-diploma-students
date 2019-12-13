3ac8ab60fe4847140905fa16a99c0d36cd01d797
Nicolas Herbaut <56995515+nzayati@users.noreply.github.com>
2019-12-13 07:06:25 +0100

    webapp

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -66,7 +66,7 @@ public class Main {
 		Iterables.addAll(students, repo);
 
 		for (int i = 0; i < students.size(); i++) {
-			if (students.get(i).getId== studentId) {
+			if (students.get(i).getId()== studentId) {
 				return students.get(i);
 			}
 		}
