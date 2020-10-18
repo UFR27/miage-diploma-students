846f29acb0e9b0295063b250594c1164f2054d16
Rebecca <rebeccagnt@orange.fr>
2020-10-18 17:10:05 +0200

    SonarLint master

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaFileAdapter.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaFileAdapter.java
@@ -3,11 +3,7 @@ package fr.pantheonsorbonne.miage;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
-
 import com.google.common.io.ByteStreams;
-
-import fr.pantheonsorbonne.miage.diploma.DiplomaSnippet;
 
 public class DiplomaFileAdapter extends FileGenerator<AbstractDiplomaGenerator> {
 
@@ -24,8 +20,9 @@ public class DiplomaFileAdapter extends FileGenerator<AbstractDiplomaGenerator> 
 			is.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-			throw new RuntimeException("failed to write diploma file", e);
-		}
+			throw new FailedException("failed to write diploma file", e);
+		} catch (FailedException e) {
+			e.getMessage();
 	}
-
+	}
 }
--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		adapter.generateFile("temp_diploma.pdf");
 				
 				
-
+		
 	}
 
 }
