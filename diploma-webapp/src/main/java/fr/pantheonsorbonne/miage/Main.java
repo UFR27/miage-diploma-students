c4792df42e880206b78be16c9c46a08587f7d443
Léa Da Costa <lea.dacosta@icloud.com>
2020-10-21 13:25:55 +0200

    really update dependencies + fix errors in diploma-webapp Main

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -1,35 +1,22 @@
 package fr.pantheonsorbonne.miage;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
+import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
 import java.util.NoSuchElementException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.servlet.Servlet;
-import javax.servlet.ServletRegistration;
 
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
-
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
 
 /**
@@ -40,7 +27,15 @@ public class Main {
 	public static final String HOST = "localhost";
 	public static final int PORT = 7000;
 	private static final Logger logger = Logger.getLogger(Main.class.getName());
-	private static StudentRepository studentRepo = StudentRepository.withDB("src/main/resources/students.db");
+	private static StudentRepository studentRepo;
+
+	{
+		try {
+			studentRepo = StudentRepository.withDB("src/main/resources/students.db");
+		} catch(NoSuchFileException e) {
+			throw new RuntimeException("failed to find file", e);
+		}
+	}
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
 
@@ -50,11 +45,11 @@ public class Main {
 
 		try {
 			server.start();
-			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
-			System.out.println("Press any key to stop the server...");
+			Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
+			Main.logger.log(Level.INFO, "Press any key to stop the server...");
 			System.in.read();
 		} catch (Exception e) {
-			System.err.println(e);
+			Main.logger.log(Level.SEVERE, e.getMessage());
 		}
 	}
 
@@ -72,20 +67,18 @@ public class Main {
 		throw new NoSuchElementException();
 	}
 
-	protected static void handleResponse(Response response, int studentId) throws IOException {
+	protected static void handleResponse(Response response, int studentId) throws IOException, DiplomaNotGeneratedException {
 		response.setContentType("application/pdf");
 
 		Student student = getStudentData(studentId, studentRepo);
 
-		EncryptedDiplomaGeneratorDecorator generator = EncryptedDiplomaGeneratorDecorator(new MiageDiplomaGenerator(student));
+		EncryptedDiplomaGeneratorDecorator generator = new EncryptedDiplomaGeneratorDecorator(new MiageDiplomaGenerator(student), student.getPassword());
 
 		try (InputStream is = generator.getContent()) {
 			try (NIOOutputStream os = response.createOutputStream()) {
 				ByteStreams.copy(is, os);
 			}
 		}
-
-		return;
 	}
 
 	protected static void addDiplomaPath(HttpServer server, String path) {
