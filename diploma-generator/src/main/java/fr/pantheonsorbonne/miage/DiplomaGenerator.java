2c5593e287dd02c9889fbc7ae0d0ab3ef88ddc35
said kabene <saidkabene@euniv-paris1.com>
2020-10-21 17:55:06 +0200

    release

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaGenerator.java
@@ -14,7 +14,10 @@ public interface DiplomaGenerator {
 	 * Generate the diploma in a Stream
 	 * 
 	 * @return
+	 * @throws failedToGenerateException 
+	 * @throws OtherRuntimeException 
 	 */
-	InputStream getContent();
+	InputStream getContent() throws failedToGenerateException, OtherRuntimeException;
+
 
 }
--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/FileGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/FileGenerator.java
@@ -6,13 +6,13 @@ package fr.pantheonsorbonne.miage;
  * @author nherbaut
  *
  */
-public abstract class FileGenerator<GENERATOR extends DiplomaGenerator> {
+public abstract class FileGenerator<S extends DiplomaGenerator> {
 
-	protected final GENERATOR generator;
+	protected final S generator;
 
-	public FileGenerator(GENERATOR t) {
+	public FileGenerator(S t) {
 		this.generator = t;
 	}
 
-	public abstract void generateFile(String outputFile);
+	public abstract void generateFile(String outputFile) throws FailedException;
 }
