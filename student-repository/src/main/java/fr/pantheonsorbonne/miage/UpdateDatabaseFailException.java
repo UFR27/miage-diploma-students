06f074e09100aa4137d8c7a5627690f7a3740c0b
RyanVernex76 <ryanvernex@yahoo.fr>
2020-11-13 11:24:21 +0100

    SonarLint Fix on student-repository

---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/UpdateDatabaseFailException.java
@@ -0,0 +1,9 @@
+package fr.pantheonsorbonne.miage;
+
+public class UpdateDatabaseFailException extends RuntimeException {
+
+	public UpdateDatabaseFailException(String message) {
+		super(message);
+	}
+
+}
