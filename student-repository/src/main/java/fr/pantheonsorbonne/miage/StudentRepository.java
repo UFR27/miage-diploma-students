3fd855c877237b061a30b2d725763f41390091b4
Sabri Abdennader <sebros02@gmail.com>
2020-11-13 14:20:39 +0100

    fix(miageDiplomaStudents): correction des autres erreurs sonarLint : generator et currentIterator

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -20,7 +20,7 @@ import org.apache.commons.csv.CSVRecord;
 public class StudentRepository implements Iterable<Student> {
 
 	private String db;
-	private java.util.Iterator<Student> currentIterator = null;
+
 
 	private StudentRepository(String db) {
 		this.db = db;
@@ -61,13 +61,13 @@ public class StudentRepository implements Iterable<Student> {
 	@Override
 	public java.util.Iterator<Student> iterator() {
 		try (FileReader reader = new FileReader(this.db)) {
-			
+			java.util.Iterator<Student> currentIterator = null;
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
-			this.currentIterator = parser.getRecords().stream()
-					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
-					.map(c -> (Student) c).iterator();
-			return this.currentIterator;
+			currentIterator = parser.getRecords().stream()
+					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
+					.map(c -> c).iterator();
+			return currentIterator;
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
