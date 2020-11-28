7db3cacc4909f606f5831a754158c4cd9189d98e
Gwendoline Bassette <gwendorose@gmail.com>
2020-11-28 13:18:56 +0100

    encryption

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/WriteDiplomaException.java
@@ -0,0 +1,11 @@
+package fr.pantheonsorbonne.miage;
+
+import java.io.IOException;
+
+public class WriteDiplomaException extends RuntimeException {
+
+	public WriteDiplomaException(String string, IOException e) {
+		super(string,e);
+	}
+	
+}
