348b8cb62a9bbdc53600336e9f03652e38e09c26
Lin Alix <linalix19@gmail.com>
2021-11-22 18:34:32 +0100

    Qualité du code

---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/UpdateException.java
@@ -0,0 +1,14 @@
+package fr.pantheonsorbonne.miage;
+
+public class UpdateException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UpdateException(String string) {
+		super(string);
+	}
+
+}
