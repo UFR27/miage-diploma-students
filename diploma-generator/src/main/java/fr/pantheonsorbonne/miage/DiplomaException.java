e5ad2e97563ae1ab0576d475d5082d2c10372108
Florent Zheng <florentzheng01@mail.com>
2021-12-06 14:45:25 +0100

    sonarlint code quality

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaException.java
@@ -0,0 +1,13 @@
+package fr.pantheonsorbonne.miage;
+
+public class DiplomaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DiplomaException(String s, Exception e) {
+		super(s, e);
+	}
+}
---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/GenerationException.java
@@ -0,0 +1,14 @@
+package fr.pantheonsorbonne.miage;
+
+public class GenerationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GenerationException(String s, Exception e) {
+		super(s, e);
+	}
+
+}
