a508d70ea5c741b453bd7ede84d76ff0e0b8e84e
hadjourmohand <hadjourmohandarezki@gmail.com>
2021-12-07 00:33:22 +0100

    SonarLint + merge Conflict

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -2,34 +2,21 @@ package fr.pantheonsorbonne.miage;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
 import java.util.NoSuchElementException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletRegistration;
 
 import org.glassfish.grizzly.http.io.NIOOutputStream;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
-import org.glassfish.grizzly.http.util.ContentType;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
 
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
 
 /**
@@ -52,11 +39,12 @@ public class Main {
 
 		{
 			server.start();
-			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
-			System.out.println("Press any key to stop the server...");
+			String uri = "http://localhost:8080/home";
+			java.awt.Desktop.getDesktop().browse(new URI(uri));
+			logger.log(Level.INFO,"Press any key to stop the server...");
 			System.in.read();
 		} catch (Exception e) {
-			System.err.println(e);
+			logger.log(Level.SEVERE,e.toString());
 		}
 	}
 
@@ -88,14 +76,14 @@ public class Main {
 			}
 
 		}
-		return;
+		
 	}
 
 	protected static void addDiplomaPath(HttpServer server, String path) {
 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
 			public void service(Request request, Response response) throws Exception {
 				// get the id of the student
-				int id = Integer.parseInt(request.getPathInfo().substring(1));
+				int id = Integer.parseInt(request.getPathInfo().substring(1))-1;
 
 				handleResponse(response, id);
 				response.setContentType("text/html; charset=utf-8");
@@ -114,8 +102,7 @@ public class Main {
 				sb.append("<!DOCTYPE html><head><meta charset='utf-8'></head><body><h1>Liste des diplômés</h1><ul>");
 				for (Student stu : StudentRepository.withDB("src/main/resources/students.db")) {
 					sb.append("<li>");
-					sb.append(
-							"<a href='/diploma/" + stu.getId() + "'>" + stu.getTitle() + ' ' + stu.getName() + "</a>");
+					sb.append("<a href='/diploma/" + stu.getId() + "'>" + stu.getTitle() + ' ' + stu.getName() + "</a>");
 					sb.append("</li>");
 				}
 
