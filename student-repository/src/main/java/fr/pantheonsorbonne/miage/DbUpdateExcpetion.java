9fe952db1c43518cd408de37d8a5cc17678fbd17
nabdi23 <neila.abdi23@gmail.com>
2021-12-07 18:51:44 +0100

    SonarLint

---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/DbUpdateExcpetion.java
@@ -0,0 +1,14 @@
+package fr.pantheonsorbonne.miage;
+
+public class DbUpdateExcpetion extends RuntimeException{
+
+	public DbUpdateExcpetion(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
