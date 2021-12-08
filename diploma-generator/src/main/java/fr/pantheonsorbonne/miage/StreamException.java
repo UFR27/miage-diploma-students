5ee1da286612da39865df78a15368b2f9e2cca60
Ishaac <ishac.hadj@yahoo.com>
2021-12-08 21:32:02 +0100

    correction de SonarLint

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/StreamException.java
@@ -0,0 +1,11 @@
+package fr.pantheonsorbonne.miage;
+
+public class StreamException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public StreamException(String s, Exception e) {
+		super(s, e);
+	}
+
+} 
