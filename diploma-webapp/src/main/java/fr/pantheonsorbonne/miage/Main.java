34338486f9d486f77e638cb74187c5eb2012724a
anhthunguyen-iutlr <anhhthunguyen83@gmail.com>
2021-12-08 21:20:38 +0100

    fix issue #3

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -65,9 +65,16 @@ public class Main {
 		ArrayList<Student> students = new ArrayList<>();
 		Iterables.addAll(students, repo);
 
-		for (int i = 0; i < students.size(); i++) {
+		/*for (int i = 0; i < students.size(); i++) {
 			if (i == studentId) {
 				return students.get(i);
+			}
+		}*/
+		
+		for (Student currStudent:students) {
+			int id = currStudent.getId();
+			if (studentId == id) {
+				return currStudent;
 			}
 		}
 
@@ -88,7 +95,7 @@ public class Main {
 			}
 
 		}
-		return;
+		//return;
 	}
 
 	protected static void addDiplomaPath(HttpServer server, String path) {
