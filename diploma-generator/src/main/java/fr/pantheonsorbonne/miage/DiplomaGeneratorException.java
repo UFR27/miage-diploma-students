047a3a311865ebc61f77ae41f1b5196faea1a7c3
MERABET Yasmine <yasminemrt14@gmail.com>
2021-12-10 15:45:10 +0100

    encryption

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaGeneratorException.java
@@ -0,0 +1,8 @@
+package fr.pantheonsorbonne.miage;
+
+public class DiplomaGeneratorException extends RuntimeException {
+
+    public DiplomaGeneratorException(String m, Exception e){
+        super(m,e);
+    }
+}
