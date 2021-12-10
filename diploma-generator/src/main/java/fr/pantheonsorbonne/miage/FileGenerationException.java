800b56ff3a78ae4499baf50b6c3518c38450f108
NicolasDrain <nicodrain@hotmail.fr>
2021-12-10 02:11:14 +0100

    Sonarlint

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/FileGenerationException.java
@@ -0,0 +1,14 @@
+package fr.pantheonsorbonne.miage;
+
+public class FileGenerationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FileGenerationException(String s, Exception e) {
+		super(s, e);
+	}
+
+}
---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/UpdateException.java
@@ -0,0 +1,14 @@
+package fr.pantheonsorbonne.miage;
+
+public class UpdateException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UpdateException(String s, Exception e) {
+		super(s, e);
+	}
+
+}
