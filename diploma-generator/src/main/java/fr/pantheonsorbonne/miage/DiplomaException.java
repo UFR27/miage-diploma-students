0705ce4492b14c943a02116fc58a2d1cd5bf458b
EricHan94 <erichan94200@gmail.com>
2021-12-11 23:17:31 +0100

    SonarLint amélioration qualité code

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/DiplomaException.java
@@ -0,0 +1,20 @@
+package fr.pantheonsorbonne.miage;
+
+import java.io.IOException;
+
+public class DiplomaException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DiplomaException(String string, IOException e) {
+
+	}
+
+	public DiplomaException(String string, Exception e) {
+
+	}
+
+} 
