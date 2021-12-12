0a2c7030a273619befbf678cd406b4d27f56fc3e
Teya Didi <dteya@yahoo.com>
2021-12-12 21:33:13 +0100

    main fix

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -14,6 +14,8 @@ import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+
 
 public class StudentRepository implements Iterable<Student> {
 
@@ -32,7 +34,7 @@ public class StudentRepository implements Iterable<Student> {
 
 	public static List<String> toReccord(Student stu) {
 
-		return Arrays.asList("" + stu.getId(), stu.getName(), stu.getTitle(),stu.getPassword());
+		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId(),stu.getPassword());
 	}
 
 	public StudentRepository add(Student s) throws UpdateFail {
