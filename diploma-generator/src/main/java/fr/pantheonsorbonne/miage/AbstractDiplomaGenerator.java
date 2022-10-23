acffef10f955fc02d7623408c3c1d0225f7cb3a1
Aurélie <Aurlie>
2022-10-23 17:08:23 +0200

    corrections Sonar

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
@@ -8,9 +8,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.HashSet;
 
-import com.google.common.io.ByteStreams;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
@@ -21,9 +19,9 @@ import fr.pantheonsorbonne.miage.diploma.DiplomaSnippet;
 
 public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 
-	private Collection<DiplomaSnippet> snippets = new HashSet<>();
+	
 
-	public AbstractDiplomaGenerator() {
+	protected AbstractDiplomaGenerator() {
 		super();
 		
 
@@ -34,7 +32,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 	 * 
 	 * @return
 	 */
-	abstract protected Collection<DiplomaSnippet> getDiplomaSnippets();
+	protected abstract Collection<DiplomaSnippet> getDiplomaSnippets();
 
 	/*
 	 * (non-Javadoc)
@@ -52,7 +50,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 
 		} catch (IOException e) {
 
-			throw new RuntimeException("failed to generate the file to stream to", e);
+			throw new UnsupportedOperationException("failed to generate the file to stream to", e);
 		}
 
 	}
@@ -76,7 +74,7 @@ public abstract class AbstractDiplomaGenerator implements DiplomaGenerator {
 			document.add(Image.getInstance(image.toAbsolutePath().toString()));
 
 		} catch (DocumentException | IOException e) {
-			throw new RuntimeException("failed to generate Document", e);
+			throw new UnsupportedOperationException("failed to generate Document", e);
 		} finally {
 			document.close();
 		}
