94a17a16f762dcd7b771cd2668ef4f73718c5bc2
kdrici <drici.katia@gmail.com>
2022-10-23 20:49:25 +0200

    chnagement webapp

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -64,13 +64,12 @@ public class Main {
 		// create an arrayList of the students, because iterables are too hard
 		ArrayList<Student> students = new ArrayList<>();
 		Iterables.addAll(students, repo);
-
-		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
-				return students.get(i);
+		for (Student student : students) {
+			if (student.getId() == studentId) {
+				return student;
 			}
 		}
-
+ 
 		throw new NoSuchElementException();
 
 	}
