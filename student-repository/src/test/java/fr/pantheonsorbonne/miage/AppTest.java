66620f38ee261958913890162eb3a7fc258dade5
Kiraly23 <risacheradrien@gmail.com>
2022-10-24 12:06:58 +0200

    Test

--- student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
+++ student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
@@ -24,8 +24,8 @@ public class AppTest {
 	public void shouldAnswerWithTrue() throws IOException {
 		File tempDB = Files.createTempFile("prefix", ".csv").toFile();
 		FileWriter fw = new FileWriter(tempDB);
-		fw.write("1,Nicolas,Dr.\n");
-		fw.write("1,Francois,M.\n");
+		fw.write("Nicolas,Dr.,1\n");
+		fw.write("Francois,M.,2\n");
 		fw.close();
 
 		assertEquals(2, Iterables.size(StudentRepository.withDB(tempDB.toString())));
@@ -36,7 +36,7 @@ public class AppTest {
 		assertEquals("Dr.", nicolas.getTitle());
 		assertEquals(1, nicolas.getId());
 
-		Student francois = Iterables.get(StudentRepository.withDB(tempDB.toString()), 0);
+		Student francois = Iterables.get(StudentRepository.withDB(tempDB.toString()), 1);
 
 		assertEquals("Francois", francois.getName());
 		assertEquals("M.", francois.getTitle());
