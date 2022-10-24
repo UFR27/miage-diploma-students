dec4da3597d121b2220ecc27c8d3223ad0b071f3
BladeBuru <72576887+BladeBuru@users.noreply.github.com>
2022-10-24 08:07:23 +0200

    Fixe des bugs

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -65,12 +65,12 @@ public class StudentRepository implements Iterable<Student> {
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
 			this.currentIterator = parser.getRecords().stream()
 					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
-					.map(c ->  c).iterator();
+					.map(Student.class::cast).iterator();
 			return this.currentIterator;
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
-			return Collections.EMPTY_SET.iterator();
+			return Collections.emptyIterator();
 		}
 	}
 
