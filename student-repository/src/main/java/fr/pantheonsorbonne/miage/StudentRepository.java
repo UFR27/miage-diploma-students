2a9af94426a112e8c48939ddcd3e3f1938f0c947
Myriam1501 <myriamo93500@gmail.com>
2022-12-28 20:15:01 +0100

    new branch and add password for student

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -20,15 +20,19 @@ public class StudentRepository implements Iterable<Student> {
 	private String db;
 	private StudentRepository(String db) {
 		this.db = db;
-	};
+	}
 
 	public static StudentRepository withDB(String db) {
+		/* 
+		if (!Files.exists(Paths.get(db))) {
+			throw new RuntimeException("failed to find" + Paths.get(db).toAbsolutePath().toString());
+		}*/
 		return new StudentRepository(db);
 	}
 
 	public static List<String> toReccord(Student stu) {
 
-		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId());
+		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId(),stu.getPassword());
 	}
 
 	public StudentRepository add(Student s) {
@@ -56,15 +60,13 @@ public class StudentRepository implements Iterable<Student> {
 
 	@Override
 	public java.util.Iterator<Student> iterator() {
-		java.util.Iterator<Student> currentIterator = null;
 		try (FileReader reader = new FileReader(this.db)) {
 			
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
-			currentIterator = parser.getRecords().stream()
- 					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
- 					.map(c -> c).iterator();
- 			return currentIterator;
+			return parser.getRecords().stream()
+			.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
+			.map(c -> (Student) c).iterator();
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
