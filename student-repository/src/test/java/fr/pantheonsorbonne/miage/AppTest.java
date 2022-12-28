f97324edbdc25842e34924b0789e17fcab3f9189
Myriam1501 <myriamo93500@gmail.com>
2022-12-28 21:39:16 +0100

    update test.java

--- student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
+++ student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
@@ -1,5 +1,51 @@
 package fr.pantheonsorbonne.miage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.Iterables;
+
+/**
+ * Unit test for simple App.
+ */
 public class AppTest {
-	
+
+	/**
+	 * Rigorous Test :-)
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void shouldAnswerWithTrue() throws IOException {
+		File tempDB = Files.createTempFile("prefix", ".csv").toFile();
+		FileWriter fw = new FileWriter(tempDB);
+		fw.write("Nicolas Herbaut, Dr,1,nicoHer\n");
+		fw.write("Mohamed Tebib, M.,2,mohaTeb\n");
+		fw.close();
+
+		assertEquals(2, Iterables.size(StudentRepository.withDB(tempDB.toString())));
+
+		Student nicolas = Iterables.get(StudentRepository.withDB(tempDB.toString()), 0);
+
+		assertEquals("Nicolas", nicolas.getName());
+		assertEquals("Dr.", nicolas.getTitle());
+		assertEquals(1, nicolas.getId());
+		assertEquals("nicoHer", nicolas.getPassword());
+
+		Student francois = Iterables.get(StudentRepository.withDB(tempDB.toString()), 0);
+
+		assertEquals("Francois", francois.getName());
+		assertEquals("M.", francois.getTitle());
+		assertEquals(2, francois.getId());
+		assertEquals("mohaTeb", francois.getPassword());
+
+	}
+
+
 }
