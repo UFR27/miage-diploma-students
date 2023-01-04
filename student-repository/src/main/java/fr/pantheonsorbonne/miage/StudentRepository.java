3dbfa82ba346790f6717ceece87e9df6b6fb7c90
Aurélie <Aurlie>
2023-01-04 18:23:37 +0100

    add main

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -59,16 +59,15 @@ public class StudentRepository implements Iterable<Student> {
 	}
 
 	@Override
-	public java.util.Iterator<Student> iterator() {
+	public Iterator<Student> iterator() {
 		try (FileReader reader = new FileReader(this.db)) {
 			
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
 			
-			Iterator<Student> currentIterator = parser.getRecords().stream()
-				.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
-				.map(c -> c).iterator();
-			return currentIterator;
+			return parser.getRecords().stream()
+					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
+					.map(c -> c).iterator();
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
