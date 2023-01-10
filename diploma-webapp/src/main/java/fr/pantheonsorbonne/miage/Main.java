753f0fc2767b3ada63c3270cbd109800602972e6
Myriam1501 <myriamo93500@gmail.com>
2023-01-10 21:33:01 +0100

    add pom

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -1,4 +1,5 @@
-package fr.pantheonsorbonne.miage;
+
+ package fr.pantheonsorbonne.miage;
 
  import java.io.IOException;
  import java.io.InputStream;
@@ -37,93 +38,93 @@ package fr.pantheonsorbonne.miage;
   *
   */
  public class Main {
- 	public static final String HOST = "localhost";
- 	public static final int PORT = 7000;
- 	private static final Logger logger = Logger.getLogger(Main.class.getName());
- 	private static StudentRepository studentRepo = StudentRepository.withDB("src/main/resources/students.db");
+	 public static final String HOST = "localhost";
+	 public static final int PORT = 7000;
+	 private static final Logger logger = Logger.getLogger(Main.class.getName());
+	 private static StudentRepository studentRepo = StudentRepository.withDB("src/main/resources/students.db");
 
- 	public static void main(String[] args) throws IOException, URISyntaxException {
+	 public static void main(String[] args) throws IOException, URISyntaxException {
 
- 		HttpServer server = HttpServer.createSimpleServer();
- 		addRootPath(server, "/home");
- 		addDiplomaPath(server, "/diploma/*");
+		 HttpServer server = HttpServer.createSimpleServer();
+		 addRootPath(server, "/home");
+		 addDiplomaPath(server, "/diploma/*");
 
- 		try
+		 try
 
- 		{
- 			server.start();
- 			java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
- 			System.out.println("Press any key to stop the server...");
- 			System.in.read();
- 		} catch (Exception e) {
- 			System.err.println(e);
- 		}
- 	}
+		 {
+			 server.start();
+			 java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/home"));
+			 System.out.println("Press any key to stop the server...");
+			 System.in.read();
+		 } catch (Exception e) {
+			 System.err.println(e);
+		 }
+	 }
 
- 	protected static Student getStudentData(int studentId, StudentRepository repo) {
- 		// create an arrayList of the students, because iterables are too hard
- 		ArrayList<Student> students = new ArrayList<>();
- 		Iterables.addAll(students, repo);
+	 protected static Student getStudentData(int studentId, StudentRepository repo) {
+		 // create an arrayList of the students, because iterables are too hard
+		 ArrayList<Student> students = new ArrayList<>();
+		 Iterables.addAll(students, repo);
 
- 		for (int i = 0; i < students.size(); i++) {
- 			if (i == studentId) {
- 				return students.get(i);
- 			}
- 		}
+		 for (int i = 0; i < students.size(); i++) {
+			 if (i == studentId) {
+				 return students.get(i);
+			 }
+		 }
 
- 		throw new NoSuchElementException();
+		 throw new NoSuchElementException();
 
- 	}
+	 }
 
- 	protected static void handleResponse(Response response, int studentId) throws IOException {
+	 protected static void handleResponse(Response response, int studentId) throws IOException {
 
- 		response.setContentType("application/pdf");
+		 response.setContentType("application/pdf");
 
- 		Student student = getStudentData(studentId, studentRepo);
+		 Student student = getStudentData(studentId, studentRepo);
 
- 		DiplomaGenerator generator = new MiageDiplomaGenerator(student);
- 		try (InputStream is = generator.getContent()) {
- 			try (NIOOutputStream os = response.createOutputStream()) {
- 				ByteStreams.copy(is, os);
- 			}
+		 DiplomaGenerator generator = new MiageDiplomaGenerator(student);
+		 try (InputStream is = generator.getContent()) {
+			 try (NIOOutputStream os = response.createOutputStream()) {
+				 ByteStreams.copy(is, os);
+			 }
 
- 		}
- 		return;
- 	}
+		 }
+		 return;
+	 }
 
- 	protected static void addDiplomaPath(HttpServer server, String path) {
- 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
- 			public void service(Request request, Response response) throws Exception {
- 				// get the id of the student
- 				int id = Integer.parseInt(request.getPathInfo().substring(1)) - 1;
+	 protected static void addDiplomaPath(HttpServer server, String path) {
+		 server.getServerConfiguration().addHttpHandler(new HttpHandler() {
+			 public void service(Request request, Response response) throws Exception {
+				 // get the id of the student
+				 int id = Integer.parseInt(request.getPathInfo().substring(1)) - 1;
 
- 				handleResponse(response, id);
- 				response.setContentType("text/html; charset=utf-8");
- 				response.setStatus(404);
- 				response.getWriter().write("Erreur 404 : Le diplome n'existe pas pour cet utilisateur");
+				 handleResponse(response, id);
+				 response.setContentType("text/html; charset=utf-8");
+				 response.setStatus(404);
+				 response.getWriter().write("Erreur 404 : Le diplome n'existe pas pour cet utilisateur");
 
- 			}
- 		}, path);
- 	}
+			 }
+		 }, path);
+	 }
 
- 	protected static void addRootPath(HttpServer server, String path) {
- 		server.getServerConfiguration().addHttpHandler(new HttpHandler() {
- 			public void service(Request request, Response response) throws Exception {
+	 protected static void addRootPath(HttpServer server, String path) {
+		 server.getServerConfiguration().addHttpHandler(new HttpHandler() {
+			 public void service(Request request, Response response) throws Exception {
 
- 				StringBuilder sb = new StringBuilder();
- 				sb.append("<!DOCTYPE html><head><meta charset='utf-8'></head><body><h1>Liste des diplômés</h1><ul>");
- 				for (Student stu : StudentRepository.withDB("src/main/resources/students.db")) {
- 					sb.append("<li>");
- 					sb.append(
- 							"<a href='/diploma/" + stu.getId() + "'>" + stu.getTitle() + ' ' + stu.getName() + "</a>");
- 					sb.append("</li>");
- 				}
+				 StringBuilder sb = new StringBuilder();
+				 sb.append("<!DOCTYPE html><head><meta charset='utf-8'></head><body><h1>Liste des diplômés</h1><ul>");
+				 for (Student stu : StudentRepository.withDB("src/main/resources/students.db")) {
+					 sb.append("<li>");
+					 sb.append(
+							 "<a href='/diploma/" + stu.getId() + "'>" + stu.getTitle() + ' ' + stu.getName() + "</a>");
+					 sb.append("</li>");
+				 }
 
- 				sb.append("</ul></body></html>");
- 				response.setContentType("text/html; charset=utf-8");
- 				response.setContentLength(sb.length());
- 				response.getWriter().write(sb.toString());
- 			}
- 		}, path);
- 	}
+				 sb.append("</ul></body></html>");
+				 response.setContentType("text/html; charset=utf-8");
+				 response.setContentLength(sb.length());
+				 response.getWriter().write(sb.toString());
+			 }
+		 }, path);
+	 }
  }
