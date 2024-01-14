dd30b83e041b5a6c042f16e28a54e298c25f5705
adrianjz02 <adr.jimenez.ian@gmail.com>
2024-01-14 23:07:56 +0100

    modified:   diploma-generator/src/test/java/fr/pantheonsorbonne/miage/EncryptedDiplomaGeneratorTest.java 	modified:   student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java

--- diploma-generator/src/test/java/fr/pantheonsorbonne/miage/EncryptedDiplomaGeneratorTest.java
+++ diploma-generator/src/test/java/fr/pantheonsorbonne/miage/EncryptedDiplomaGeneratorTest.java
@@ -18,6 +18,7 @@ import com.itextpdf.text.pdf.PdfStamper;
 public class EncryptedDiplomaGeneratorTest extends DiplomaGeneratorTest {
 
 	private final class EncryptedPdfReader extends PdfReader {
+		
 		private EncryptedPdfReader(String filename, String password) throws IOException {
 			super(filename);
 			this.password = password.getBytes();
--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -64,11 +64,11 @@ public class StudentRepository implements Iterable<Student> {
 		try (FileReader reader = new FileReader(this.db)) {
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
-			this.currentIterator = parser.getRecords().stream()
+			currentIterator = parser.getRecords().stream()
 					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1),
 							reccord.get(3)))
-					.map(c -> (Student) c).iterator();
-			return this.currentIterator;
+					.map(c -> c).iterator();
+			return currentIterator;
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
