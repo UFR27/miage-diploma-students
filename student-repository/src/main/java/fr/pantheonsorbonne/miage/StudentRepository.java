21ed98ee4f60e9653be8828aeb917f4d8ed17179
shkslxiv <kimseonho133@gmail.com>
2024-01-15 07:24:11 +0900

    modified:   student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java 	modified:   student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -5,8 +5,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.commons.csv.CSVFormat;
@@ -23,9 +25,14 @@ public class StudentRepository implements Iterable<Student> {
 
 	public static StudentRepository withDB(String db) {
 		if (!Files.exists(Paths.get(db))) {
-			throw new RuntimeException("failed to find" + Paths.get(db).toAbsolutePath().toString());
+			throw new UnsupportedOperationException("failed to find" + Paths.get(db).toAbsolutePath().toString());
 		}
 		return new StudentRepository(db);
+	}
+
+	public static List<String> toReccord(Student stu) {
+
+		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId(),stu.getPassword());
 	}
 
 	public StudentRepository add(Student s) {
@@ -37,7 +44,7 @@ public class StudentRepository implements Iterable<Student> {
 				try {
 					csvFilePrinter.printRecord(toReccord(student));
 				} catch (IOException e) {
-					throw new RuntimeException("failed to update db file");
+					throw new UnsupportedOperationException("failed to update db file");
 				}
 			});
 			csvFilePrinter.printRecord(toReccord(s));
@@ -51,20 +58,23 @@ public class StudentRepository implements Iterable<Student> {
 
 	}
 
+
 	@Override
 	public java.util.Iterator<Student> iterator() {
+
 		java.util.Iterator<Student> currentIterator = null;
+
 		try (FileReader reader = new FileReader(this.db)) {
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
-			this.currentIterator = parser.getRecords().stream()
-					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
-					.map(c -> (Student) c).iterator();
+			currentIterator = parser.getRecords().stream()
+					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
+					.map(c -> c).iterator();
 			return currentIterator;
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
-			return Collections.emptyListIterator();
+			return Collections.emptyIterator();
 		}
 	}
 
--- student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
+++ student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
@@ -1,6 +1,6 @@
 package fr.pantheonsorbonne.miage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.FileWriter;
