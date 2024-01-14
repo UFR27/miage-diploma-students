2a6660ee498f32e7c14d8078ab2e16561d225f0c
Mayane-C <mayanecohen2626@gmail.com>
2024-01-15 00:50:36 +0100

    issue 6

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -65,8 +65,8 @@ public class StudentRepository implements Iterable<Student> {
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
 			this.currentIterator = parser.getRecords().stream()
-					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(1), reccord.get(0), reccord.get(3)))
-					.map(c -> (Student) c).iterator();
+			.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1),
+			reccord.get(3))).map(c -> (Student) c).iterator();
 			return this.currentIterator;
 
 		} catch (IOException e) {
@@ -74,5 +74,4 @@ public class StudentRepository implements Iterable<Student> {
 			return Collections.EMPTY_SET.iterator();
 		}
 	}
-
 }
