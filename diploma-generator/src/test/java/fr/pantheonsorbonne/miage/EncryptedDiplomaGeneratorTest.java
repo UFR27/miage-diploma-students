f851234df847b22cc492b5bda08fde771597dcc4
Mayane-C <mayanecohen2626@gmail.com>
2024-01-15 16:40:03 +0100

    amelioration de la couverture  de test

--- diploma-generator/src/test/java/fr/pantheonsorbonne/miage/EncryptedDiplomaGeneratorTest.java
+++ diploma-generator/src/test/java/fr/pantheonsorbonne/miage/EncryptedDiplomaGeneratorTest.java
@@ -15,22 +15,11 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 
-public class EncryptedDiplomaGeneratorTest extends DiplomaGeneratorTest {
+class EncryptedDiplomaGeneratorTest extends DiplomaGeneratorTest {
 
-	private final class EncryptedPdfReader extends PdfReader {
-		private EncryptedPdfReader(String filename, String password) throws IOException {
-			super(filename);
-			this.password = password.getBytes();
-
-		}
-
-		public void setEncrypted(boolean encrypted) {
-			this.encrypted = encrypted;
-		}
-	}
 
 	@Test
-	public void testEncryptedPdfTest() throws IOException, DocumentException {
+	void testEncryptedPdfTest() throws IOException, DocumentException {
 
 		Student stu = new Student(1, "Nicolas", "", "nico");
 		DiplomaGenerator generator = new MiageDiplomaGenerator(stu, DiplomaGeneratorTest.currentDate);
