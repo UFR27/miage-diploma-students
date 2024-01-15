c8e61b063790fcb0370bd59dc297457dc9d08f02
Mayane-C <mayanecohen2626@gmail.com>
2024-01-15 15:50:32 +0100

    merge de releasev1 ddans master

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -16,7 +16,6 @@ import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
-import org.apache.commons.csv.CSVRecord;
 
 public class StudentRepository implements Iterable<Student> {
 
@@ -48,11 +47,8 @@ public class StudentRepository implements Iterable<Student> {
 				try {
 					csvFilePrinter.printRecord(toReccord(student));
 				} catch (IOException e) {
-<<<<<<< HEAD
 					throw new DataBaseFlawException("failed to update db file");
-=======
-					throw new RuntimeException("failed to update db file");
->>>>>>> encryption-unit-tests
+
 				}
 			});
 			csvFilePrinter.printRecord(toReccord(s));
