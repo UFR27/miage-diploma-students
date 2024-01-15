3b02960f935aa04f3affa9266e6dfb430fe16314
Abel Grossman <abeljaspergrossman@gmail.com>
2024-01-15 15:25:26 +0100

    finalReleaseV1

--- student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Logger;
 
 import org.apache.commons.csv.CSVFormat;
@@ -32,13 +31,15 @@ public class StudentRepository implements Iterable<Student> {
 	}
 
 	public static List<String> toReccord(Student stu) {
-		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId());
+
+		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId(),stu.getPassword());
 	}
 
 	public StudentRepository add(Student s) {
 		Iterator<Student> previousContent = StudentRepository.withDB(this.db).iterator();
 		try (FileWriter writer = new FileWriter(this.db)) {
 			CSVPrinter csvFilePrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+
 			previousContent.forEachRemaining(student -> {
 				try {
 					csvFilePrinter.printRecord(toReccord(student));
@@ -57,21 +58,23 @@ public class StudentRepository implements Iterable<Student> {
 
 	}
 
+
 	@Override
 	public java.util.Iterator<Student> iterator() {
+
 		java.util.Iterator<Student> currentIterator = null;
+
 		try (FileReader reader = new FileReader(this.db)) {
 
 			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
-			this.currentIterator = parser.getRecords().stream()
-					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
-					.map(c -> (Student) c).iterator();
-			return this.currentIterator;
+			currentIterator = parser.getRecords().stream()
+					.map(reccord -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1), reccord.get(3)))
+					.map(c -> c).iterator();
+			return currentIterator;
 
 		} catch (IOException e) {
 			Logger.getGlobal().info("IO PB" + e.getMessage());
-			Set<Student> emptySet = new HashSet<>();
-			return emptySet.iterator();
+			return Collections.emptyIterator();
 		}
 	}
 
